ef8e93a79ebaf9104dce1311f67c2dcb2125883a
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-05-23 12:20:10 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { Statistic, Progress, Row, Col } from "antd";
+import React from "react";
+import "./App.css";
+import "antd/dist/antd.css";
+import { CiCircleFilled } from "@ant-design/icons/";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  timer;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: new Date().getSeconds(),
+      time: new Date(),
+      percent: 0,
+      showIcon: false,
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    return {
+      percent: Math.round((state.seconds / 60) * 100),
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Statistic title="Seconds" value={this.state.seconds} />
+        <br />
+        <Row>
+          <Col span={6} offset={6}>
+            <Progress
+              type="circle"
+              strokeColor="green"
+              percent={this.state.percent}
+            />
+          </Col>
+          <Col span={6}>
+            <Progress
+              type="circle"
+              strokeColor="red"
+              percent={100 - this.state.percent}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12} offset={6}>{this.state.showIcon && <CiCircleFilled spin />}</Col>
+        </Row>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState({ seconds: new Date().getSeconds(), time: new Date() });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevState.time.getMinutes() !== this.state.time.getMinutes()) {
+      this.setState((state) => ({
+        showIcon:
+          prevState.time.getMinutes() !== this.state.time.getMinutes()
+            ? !state.showIcon
+            : state.showIcon,
+      }));
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
 }
-
-export default App;
